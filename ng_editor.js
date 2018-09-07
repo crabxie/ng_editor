337caf1d57c07e77c0baafbf1a158e04500a06e4
crab <crab68@126.com>
2018-09-08 04:06:46 +0800

    增加页面获取

--- ng_editor.js
+++ ng_editor.js
@@ -3,33 +3,58 @@
  */
 ;(function($,window,document,undefined){
     var NgEditor = function(ele, opt) {
-        var self = this;
+        var ng_self = this;
         this.$element = ele,
         this.defaults = {
             'debug': true,
             'company_id':'',
             'user_id':'',
             'account_api':'',
-            'page_api':'',
+            'build_page_api':'',
         },
         this.options = $.extend({}, this.defaults, opt);
 
         this.user_data = {'user_id':this.options.user_id,'company_id':this.options.company_id};
 
-        this.getAccount = function(opt){
-            if (this.options.account_api && this.options.company_id && this.options.user_id) {
-                this.user_post_data = $.extend({},this.user_data,opt);
-                this._load(this.options.account_api,'post',this.user_post_data,function(flag,respone){
-                    if (flag) {
-                        $('#pl_login_account').find('.pl_login_avatar').attr('src',respone.avatar);
-                        $('#pl_login_account').find('.pl_login_name').text(respone.name+' ('+respone.account+')');
-                        var group_type = respone.group_type==0 ? '主账户' :"子账户";
-                        $('#pl_login_account').find('.pl_login_group').text(group_type);
-                    }
-                });
+        this.get_account = function(opt){
+            if (ng_self.options.account_api && ng_self.options.company_id && ng_self.options.user_id) {
+                ng_self.user_post_data = $.extend({},ng_self.user_data,opt);
+                $(document).queue("ajaxRequests",
+                    ng_self._load(ng_self.options.account_api,'post',ng_self.user_post_data,function(flag,respone){
+                            if (flag) {
+                                $('#pl_login_account').find('.pl_login_avatar').attr('src',respone.avatar);
+                                $('#pl_login_account').find('.pl_login_name').text(respone.name+' ('+respone.account+')');
+                                var group_type = respone.group_type==0 ? '主账户' :"子账户";
+                                $('#pl_login_account').find('.pl_login_group').text(group_type);
+                            }
+                        })
+                );
             }
         }
+        this.get_ng_page = function() {
+            if (ng_self.options.build_page_api && ng_self.options.company_id && ng_self.options.user_id) {
+                $(document).queue("ajaxRequests",
+                    ng_self._load(ng_self.options.build_page_api,'post',ng_self.user_post_data,function(flag,respone){
+                        if (flag) {
+                            $('#ng_page').html('');
+                            for(var page_id in respone) {
+                                var html_str = [];
+                                html_str.push('<div class="ng_page_item" rel="'+page_id+'">');
+                                html_str.push('<div class="ng_page_snapshot"><img src="'+respone[page_id].snapshot+'"/></div>');
+                                html_str.push('<input type="hidden" name="ng_pages[]" value="'+page_id+'">');
+                                html_str.push('<div class="ng_page_item_title">'+respone[page_id].title+'</div>');
+                                html_str.push('</div>');
+                                $('#ng_page').append(html_str.join(''));
+                            }
+
+                        }
+                    })
+                );
+            }
+        }
+
         this.handle_tag = function(index) {
+
             $('a.ng_tag').each(function(){
                 var a_obj = this;
                 $(a_obj).removeClass('ng_btn_active');
@@ -43,6 +68,11 @@
                     $(a_obj).addClass('ng_btn_active');
                     var rel = $(a_obj).attr('rel');
                     $('#'+rel).css({'display':'block'});
+
+                    var load_func = eval('ng_self.get_'+rel);
+                    if (typeof load_func == "function") {
+                        load_func();
+                    }
                 });
             });
             $('a.ng_tag').get(index).click();
@@ -66,20 +96,20 @@
                     respone = eval('['+respone+']')[0];
                     var respone_data =respone.data;
                     if (respone.status) {
-                        if (self.options.debug) {
+                        if (ng_self.options.debug) {
                             console.log('成功');
                         }
                         cb(respone.status,respone_data);
 
                     } else {
-                        if (self.options.debug) {
+                        if (ng_self.options.debug) {
                             console.log('失败');
                         }
                     }
 
                 },
                 error:function(respone){
-                    if (self.options.debug) {
+                    if (ng_self.options.debug) {
                         console.log('发生错误');
                     }
 
@@ -94,7 +124,7 @@
                 console.log('NgEditor init');
                 console.log('mode: debug');
             }
-            this.getAccount({});
+            this.get_account({});
             this.handle_tag(0);
 
         }
